328ac67e8b2a4c17a41bfc270d592f7450975c15
Thamonwan Maneechan <47081950+thtop@users.noreply.github.com>
2019-05-06 00:11:01 +0700

    Edit space on VideoItem.js file

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -6,13 +6,13 @@ const VideoItem = ({ video, onVideoSelect }) => {
         <div className="video-item item" onClick={() => onVideoSelect(video)}>
             <img className="ui image" 
                 src={video.snippet.thumbnails.medium.url} 
-                alt={video.snippet.title} />
-
-                <div className="content">
-                    <div className="header" style={{ color: '#fff'}}>
-                        {video.snippet.title}
-                    </div>
+                alt={video.snippet.title} 
+            />
+            <div className="content">
+                <div className="header" style={{ color: '#fff'}}>
+                    {video.snippet.title}
                 </div>
+            </div>
         </div>
     )
 }
