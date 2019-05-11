ec24b32a0d92f57bd871477212b8b742feb88b0a
Thamonwan Maneechan <47081950+thtop@users.noreply.github.com>
2019-05-11 14:16:40 +0700

    Finished blog project

---
+++ blog/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostList from './PostList'
+
+const App = () => {
+    return (
+        <div className="ui container" style={{marginTop: '30px'}}>
+            <PostList />
+        </div>
+    )
+}
+
+export default App
