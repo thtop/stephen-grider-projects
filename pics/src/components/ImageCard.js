1f8d717326d4e49397db43c8c14b911bd6e952f6
Thamonwan Maneechan <47081950+thtop@users.noreply.github.com>
2019-05-19 16:07:35 +0700

    Update Image Search project (๋pics)

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -4,9 +4,7 @@ class ImageCard extends React.Component {
 
     constructor(props) {
         super(props)
-
         this.state = { spans: 0 }
-
         this.imageRef = React.createRef()
     }
 
@@ -16,21 +14,17 @@ class ImageCard extends React.Component {
 
     setSpans = () => {
         const height = this.imageRef.current.clientHeight
-
         const spans = Math.ceil(height / 10)
-
         this.setState({ spans })
     }
 
     render() {
-
-        const { alt_description, urls} = this.props.image;
-
+        const { alt_description, urls} = this.props.image
         return (
             <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img 
                     ref={this.imageRef}
-                    alt={alt_description} 
+                    alt={alt_description}
                     src={urls.regular}
                 />
             </div>
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -13,13 +13,15 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form" onSubmit={this.onFormSubmit} >
+                <form  onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input 
                             type="text" 
-                            value={this.state.term}  
-                            onChange={e => this.setState({ term: e.target.value })} />
+                            placeholder="Search..." 
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value })}
+                        />
                     </div>
                 </form>
             </div>
